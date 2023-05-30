43fd32778b2fac4b1962329182a84f6dd5fc95c3
Gustavo Antônio Soares de Oliveira <gustavo.antonio@transbetim.betim>
2023-05-30 16:38:41 -0300

    terceiro commit

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+
     let products = document.querySelector('.products');
+    let div = document.querySelector('.product')
+    const searchInput = document.querySelector(".form-control")
+    let users = []
 
-    async function fetchProducts(url) {
+    searchInput.addEventListener("input", function (e){
+
+        const value = e.target.value
+        console.log(value)
+        users.forEach( user =>{
+            const isvisible = user.title.includes(value)
+            div.style.display = "none"
+              });
+            });
+
+     async function fetchProducts(url) {
         let data = await fetch(url);
-        let response = await data.json();
-
+        let response =  await data.json();
+     
         for (let i = 0; i <= 9; i++) {
+          
+            users[i]= [response[i].title]
+           
             html = `<div class="product">
                 <img src="${response[i].image}" alt="" class="product-img" width="100" height="100" >
                 <h2 class="product-title" data-id="${response[i].id}" onclick="showProductDetails(event)">${response[i].title}</h2>
@@ -15,13 +33,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
             products.innerHTML += html;
         }
+      
     }
-
+    console.log(users)
     fetchProducts("https://fakestoreapi.com/products");
 });
 
 function showProductDetails(event) {
     const productId = event.target.getAttribute('data-id');
     // Redireciona para a página detalhes.html com o ID do produto na query string
-    window.location.href = `detalhes.html?id=${productId}`;
+    window.open(`detalhes.html?id=${productId}`);
 }
